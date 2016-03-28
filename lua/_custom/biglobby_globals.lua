27bd0a39a1f48bab3e99e7a4f04c6436b891d394
Brennan <polarathene-signup@hotmail.com>
2016-03-29 01:03:47 +1300

    Change Globals from Persist to Hook
    Have mirrored the way Beardlib uses hook_id instead of running the globals as a persist script. This lets lua classes that previously could not call the globals function work. Now all references to the player limit can be changed in once place.

--- lua/_custom/biglobby_globals.lua
+++ lua/_custom/biglobby_globals.lua
@@ -1,30 +1,32 @@
-BigLobbyGlobals = BigLobbyGlobals or class()
+if not _G.BigLobbyGlobals then
+    _G.BigLobbyGlobals = {}
 
--- I think I only need this function and can delete the rest?
-function BigLobbyGlobals:num_player_slots()
-    return 15
+    -- The new player limit is defined here, it should not be greater than
+    -- the max values set in the pdmod file.
+    BigLobbyGlobals.num_players = 14
+
+    function BigLobbyGlobals:num_player_slots()
+        return self.num_players
+    end
+
+    -- Semantic versioning
+    function BigLobbyGlobals:version()
+        return "1.0.0"
+    end
+
+
+    -- Nothing calls this anymore for the time being.
+    local log_data = true -- Can use to turn the logging on/off
+    function BigLobbyGlobals:logger(content, use_chat)
+        if log_data then
+            if not content then return end
+
+            if use_chat then
+                managers.chat:_receive_message(ChatManager.GAME, "BigLobby", content, tweak_data.system_chat_color)
+            end
+
+            log(content)
+        end
+    end
+
 end
-
-
-function BigLobbyGlobals:version()
-    return 0.4
-end
-
-
---Use global version later? Possible issue with reaching global/class functions for some classes?
-local log_data = true
-function logger(content, use_chat)
-	if log_data then
-		if not content then return end
-
-		if use_chat then
-			managers.chat:_receive_message(ChatManager.GAME, "BigLobby", content, tweak_data.system_chat_color)
-		end
-
-		log(content)
-	end
-end
-
--- Fix fps performance problems due to running this script every frame
--- 'cos I'm a nub.
-biglobby_globals=true
