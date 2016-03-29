c4a4bda5bd49ab48b4092eb8fa42a531a6519841
Anton Pupkov <pupant@gmail.com>
2016-03-28 21:32:18 -0700

    Disabled Achievements

---
+++ lua/lib/managers/_achievementmanager.lua
@@ -0,0 +1,24 @@
+function AchievmentManager:award(id)
+    --Disable achievement progress
+    return
+end
+
+function AchievmentManager:_give_reward(id, skip_exp)
+    --Disable achievement progress
+    return
+end
+
+function AchievmentManager:award_progress(stat, value)
+    --Disable achievement progress
+    return
+end
+
+function AchievmentManager:award_steam(id)
+	--Disable achievement progress
+    return
+end
+
+function AchievmentManager.steam_unlock_result(achievment)
+	--Disable achievement progress
+    return
+end
