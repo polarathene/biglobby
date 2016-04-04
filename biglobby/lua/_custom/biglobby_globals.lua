2445349b6dacdb601559563fe2c154f230c844f4
GREAT BIG BUSHY BEARD <screenguyy@gmail.com>
2016-04-04 22:00:27 +0100

    Updated menu system

--- biglobby/lua/_custom/biglobby_globals.lua
+++ biglobby/lua/_custom/biglobby_globals.lua
@@ -1,11 +1,27 @@
 if not _G.BigLobbyGlobals then
     _G.BigLobbyGlobals = {}
 
+    BigLobbyGlobals.ModPath = ModPath
+    BigLobbyGlobals.SavePath = SavePath
+    
+    BigLobbyGlobals.ClassPath = "lua/_custom/"
+    
+    BigLobbyGlobals.Classes = {
+        "menu.lua"
+    }
+    
+    for _, class in pairs(BigLobbyGlobals.Classes) do
+        dofile(BigLobbyGlobals.ModPath .. BigLobbyGlobals.ClassPath .. class)
+    end
+    
+    BigLobbyGlobals.Menu = bkin_bl__menu:new()
+    
     -- The new player limit is defined here, it should not be greater than
     -- the max values set in the pdmod file.
     -- Prefer `Global.num_players` set by BigLobby host
-    BigLobbyGlobals.num_players = Global.num_players or Global.num_players_settings or 8
-
+    -- Use Global.num_players_settings as it will either be the user option or the default value for the Player #
+    BigLobbyGlobals.num_players = Global.num_players or Global.num_players_settings
+    
     function BigLobbyGlobals:num_player_slots()
         return self.num_players
     end
