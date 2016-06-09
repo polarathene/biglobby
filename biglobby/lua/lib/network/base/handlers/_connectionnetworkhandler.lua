5657a4092a44ae25ff562946b6a2b490df9a1a3f
polarathene <polarathene@users.noreply.github.com>
2016-06-10 01:06:38 +1200

    Add missing class to mod.txt
    I either lost my changes or forgot to add networkpeer.lua to the mod.txt. This prevented biglobby__ prefix network calls from happening.

--- biglobby/lua/lib/network/base/handlers/_connectionnetworkhandler.lua
+++ biglobby/lua/lib/network/base/handlers/_connectionnetworkhandler.lua
@@ -5,6 +5,8 @@ BigLobby__ConnectionNetworkHandler = BigLobby__ConnectionNetworkHandler or class
 -- params: [ reply_id, my_peer_id, my_character, level_index, difficulty_index, state, server_character,
 -- user_id, mission, job_id_index, job_stage, alternative_job_stage, interupt_job_stage_level_index,
 -- xuid, auth_ticket, num_players, sender ]
+-- The function is effectively the same, varargs(...) are used instead to support
+-- passing an extra param when in BigLobby mode.
 function BigLobby__ConnectionNetworkHandler:join_request_reply(...)
 	if not self._verify_in_client_session() then
 		return
