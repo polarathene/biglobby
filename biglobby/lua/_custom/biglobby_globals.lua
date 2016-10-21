e63c3b12ecb0259b8c48423e5334dc4f66c4fd54
polarathene <noreply@github.com>
2016-10-21 14:55:04 +1300

    Version 2.30
    Sync to U119,  added settings and feature to increase amount of bots in lobby, fix breakage with LobbyInspect mod, Level progression fixes.

--- biglobby/lua/_custom/biglobby_globals.lua
+++ biglobby/lua/_custom/biglobby_globals.lua
@@ -59,13 +59,13 @@ if not _G.BigLobbyGlobals then
 
 	-- Semantic versioning
 	function BigLobbyGlobals:version()
-		return "2.29"
+		return "2.30"
 	end
 
 
 	-- GameVersion for matchmaking, integer is expected
 	function BigLobbyGlobals:gameversion()
-		return 229
+		return 230
 	end
 
 
