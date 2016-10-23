670e99443f50bc28c4355afc3ead3c762939ac6c
polarathene <polarathene@users.noreply.github.com>
2016-10-23 14:14:43 +1300

    Version 2.32
    Sync to U119.1 and two bugfixes to network methods that were missed for previous syncs (`from_server_sentry_gun_place_result` and `join_request_reply` difficulty).

--- biglobby/lua/_custom/biglobby_globals.lua
+++ biglobby/lua/_custom/biglobby_globals.lua
@@ -59,13 +59,13 @@ if not _G.BigLobbyGlobals then
 
 	-- Semantic versioning
 	function BigLobbyGlobals:version()
-		return "2.31"
+		return "2.32"
 	end
 
 
 	-- GameVersion for matchmaking, integer is expected
 	function BigLobbyGlobals:gameversion()
-		return 231
+		return 232
 	end
 
 
