2e46b5c941ebe3126e52e3909b9e2986485e241c
polarathene <polarathene@users.noreply.github.com>
2016-11-11 22:51:21 +1300

    Start loop from 5th peer
    Missed the inner loop bump from 1st peer to 5th as well, this caused a crash.

--- biglobby/lua/lib/managers/menu/_menulobbyrenderer.lua
+++ biglobby/lua/lib/managers/menu/_menulobbyrenderer.lua
@@ -18,7 +18,7 @@ function MenuLobbyRenderer:open(...)
 		t.free = true
 		t.kit_slots = {}
 		t.params = {}
-		for slot = 1, PlayerManager.WEAPON_SLOTS + 3 do
+		for slot = 5, PlayerManager.WEAPON_SLOTS + 3 do
 			table.insert(t.kit_slots, slot)
 		end
 		table.insert(self._player_slots, t)
