aa7353ff52d3be83c5250417c6890c64e3dbc0d2
polarathene <polarathene@users.noreply.github.com>
2016-11-13 14:51:31 +1300

    Version 2.36
    Bugfix by reverting a file to 2.32 version. The bugs caused by 2.33 were due to `self._player_slots = {}` in `_missionbriefinggui` which is avoidable but the "reduction" /hook did not provide much value. With `_menulobbyrenderer`, the issue was that `self:_entered_menu()` gets called at the end preventing an easy reduction via hook. Bad Overkill!

--- biglobby/lua/_custom/biglobby_globals.lua
+++ biglobby/lua/_custom/biglobby_globals.lua
@@ -59,13 +59,13 @@ if not _G.BigLobbyGlobals then
 
 	-- Semantic versioning
 	function BigLobbyGlobals:version()
-		return "2.35"
+		return "2.36"
 	end
 
 
 	-- GameVersion for matchmaking, integer is expected
 	function BigLobbyGlobals:gameversion()
-		return 235
+		return 236
 	end
 
 
