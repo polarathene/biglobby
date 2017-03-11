b8ae44e4c06ad52c92328eaedf2b914c2f98a51c
polarathene <polarathene@users.noreply.github.com>
2017-03-12 12:47:14 +1300

    Version 2.38
    Sync to U133.

--- biglobby/lua/_custom/biglobby_globals.lua
+++ biglobby/lua/_custom/biglobby_globals.lua
@@ -59,13 +59,13 @@ if not _G.BigLobbyGlobals then
 
 	-- Semantic versioning
 	function BigLobbyGlobals:version()
-		return "2.37"
+		return "2.38"
 	end
 
 
 	-- GameVersion for matchmaking, integer is expected
 	function BigLobbyGlobals:gameversion()
-		return 237
+		return 238
 	end
 
 
